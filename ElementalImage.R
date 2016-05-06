14c0937378d30fa6e5bf1b12afa87cad26664d83
andreas fittschen <andreas.fittschen@cornelsen-services.de>
2016-05-06 13:46:10 -0700

    Comments

--- ElementalImage.R
+++ ElementalImage.R
@@ -1,36 +1,37 @@
 library(tcltk)
 library(fields)
 
-
-
-
+# File filter for file.choose
 filters <- matrix(c("dat-files",".dat","All","*"),2,2,byrow = TRUE)
 
+# Wrapper for filechooser in windows vs. linux/mac world
 filechooser<-function(default = "", caption = "Select files", multi = TRUE, 
                       filters = NULL, index = 1) {
 	if(Sys.info()["sysname"] != "Windows") tk_choose.files(default,caption, multi, filters, index)
 	else choose.files()
 }
 	
-
+# Wrapper for dirchooser in windows vs. linux/mac world
 dirchooser<-function(default = "", caption = "Select directory") {
 	if(Sys.info()["sysname"] == "Linux") tk_choose.dir(default,caption)
 	else choose.files()
 }
 	
-
+#Only read PyMCAData once per session
 if (length(ls(pattern="^PyMCAData$"))==0) {
 	filename <- filechooser(caption="Select Data File", multi = FALSE, filters = filters)
 	PyMCAData <- read.table(filename, header=TRUE)
 }
 
+#helper to detect element and emission-lines only
 stringFilter <- function(s){!(length(grep("^((s.+\\..+)|([^.]*))$",s))>0)}
 
-
+#list of elements contained in the dat file
 elements <- data.frame(matrix(unlist(strsplit(Filter(stringFilter, names(PyMCAData)),"[.]")),ncol=2,byrow=TRUE))
 
 #periodicTable <- read.csv(tk_choose.files(),header=TRUE)
-
+ 
+#helper to translate
 translateElementLine <- function(name) {
   n1 <- sub("[.]([LM])(.)"," ~ \\1[\\2]",fixed=FALSE,
             sub("[.]Ka"," ~ K * alpha",
